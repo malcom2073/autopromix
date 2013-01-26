f461b37772b3757241fb99c727f0d8350e7a9fff
Michael Carpenter <malcom2073@gmail.com>
2013-01-26 08:18:20 -0500

    Addition of copyright notices (GPLv2)

--- apuone.cpp
+++ apuone.cpp
@@ -1,3 +1,23 @@
+/**************************************************************************
+*   Copyright (C) 2013 by Michael Carpenter (malcom2073)                  *
+*   malcom2073@gmail.com                                                  *
+*                                                                         *
+*   This file is a part of autopromix                                     *
+*                                                                         *
+*   autopromix is free software: you can redistribute it and/or modify    *
+*   it under the terms of the GNU Lesser General Public License           *
+*   vversion 2 as published by the Free Software Foundation               *
+*                                                                         *
+*   autopromix is distributed in the hope that it will be useful,         *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with autopromix.  If not, see <http://www.gnu.org/licenses/>.   *
+***************************************************************************/
+
+
 #include "apuone.h"
 
 ApuOne::ApuOne(QObject *parent) :
